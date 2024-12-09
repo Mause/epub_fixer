582fe2d6f6cb20dbffe93976188a8d2d2b06a337
Elliana May <me@mause.me>
2024-12-09 19:46:21 +0800

    that's ruff buddy

--- fix.py
+++ fix.py
@@ -4,7 +4,7 @@ import re
 from epubcheck import EpubCheck
 
 parser = ArgumentParser()
-parser.add_argument('filename', help='the file to read')
+parser.add_argument("filename", help="the file to read")
 args = parser.parse_args()
 
 result = EpubCheck(args.filename, autorun=False)
@@ -13,25 +13,25 @@ print(result.valid)
 print(result.messages)
 
 if not result.messages:
-    print('No issues found')
+    print("No issues found")
     exit(0)
 
 book = read_epub(args.filename)
-if book.title == 'Unknown Title':
-    raise ValueError('Unknown title')
+if book.title == "Unknown Title":
+    raise ValueError("Unknown title")
 
-for message in result.messages
-    if message.level == 'ERROR':
+for message in result.messages:
+    if message.level == "ERROR":
         print(message.message)
-        print(list(book.get_items_of_media_type('text/css')))
+        print(list(book.get_items_of_media_type("text/css")))
 
-        item, = book.get_items_of_media_type('text/css')
+        (item,) = book.get_items_of_media_type("text/css")
 
         item.content = re.sub(
-                r'(direction: [^;]+;)',
-                lambda *args: '',
-                item.content.decode('utf-8'),
-        ).encode('utf-8')
+            r"(direction: [^;]+;)",
+            lambda *args: "",
+            item.content.decode("utf-8"),
+        ).encode("utf-8")
 
 
-write_epub(args.filename + '.fixed.epub', book)
+write_epub(args.filename + ".fixed.epub", book)
