63911e2483452d0815c6762277290121ed29e3e9
Elliana May <me@mause.me>
2024-12-09 20:27:35 +0800

    rework logs

--- fix.py
+++ fix.py
@@ -7,10 +7,9 @@ parser = ArgumentParser()
 parser.add_argument("filename", help="the file to read")
 args = parser.parse_args()
 
+print("checking for issues")
 result = EpubCheck(args.filename, autorun=False)
 result.run()
-print(result.valid)
-print(result.messages)
 
 if not result.messages:
     print("No issues found")
@@ -21,10 +20,8 @@ if book.title == "Unknown Title":
     raise ValueError("Unknown title")
 
 for message in result.messages:
+    print(message)
     if message.level == "ERROR":
-        print(message.message)
-        print(list(book.get_items_of_media_type("text/css")))
-
         (item,) = book.get_items_of_media_type("text/css")
 
         item.content = re.sub(
