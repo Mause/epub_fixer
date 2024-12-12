0224e80bd1ae83aa156f1a36f95485f04b5e4aa1
Elliana May <me@mause.me>
2024-12-12 03:35:40 +0000

    add reusable function

--- src/epub_fixer/test_cli.py
+++ src/epub_fixer/test_cli.py
@@ -1,4 +1,5 @@
 from os.path import basename
+from pathlib import Path
 from urllib.request import urlretrieve
 
 from click.testing import CliRunner
@@ -10,6 +11,10 @@ from .__main__ import epub_fixer
 @fixture
 def happy_epub(tmp_path):
     url = "https://github.com/IDPF/epub3-samples/releases/download/20230704/accessible_epub_3.epub"
+    return get_epub(tmp_path, url)
+
+
+def get_epub(tmp_path: Path, url: str) -> Path:
     out = tmp_path / basename(url)
     urlretrieve(url, out)
     return out
