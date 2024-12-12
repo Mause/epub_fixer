6556d6ab65e3992e9b1be07b725ced8f77926762
Elliana May <me@mause.me>
2024-12-12 03:39:10 +0000

    add unhappy test

--- src/epub_fixer/test_cli.py
+++ src/epub_fixer/test_cli.py
@@ -26,3 +26,19 @@ def test_all_good(happy_epub):
     result = runner.invoke(epub_fixer, [str(happy_epub)])
     assert result.exit_code == 0
     assert result.output == "No issues found\n"
+
+
+@fixture
+def unhappy_epub(tmp_path):
+    return get_epub(
+        tmp_path,
+        "https://github.com/robotmedia/iBooks-ePub-Samples/raw/refs/heads/master/JQuery-Hello-World.epub",
+    )
+
+
+def test_issues(unhappy_epub, snapshot):
+    runner = CliRunner()
+
+    result = runner.invoke(epub_fixer, [str(unhappy_epub)])
+    assert result.exit_code == 1
+    assert result.output == snapshot
